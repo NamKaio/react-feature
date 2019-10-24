aed635688f4aa8e65964e31b5f1c6aa08b8f5798
NamKaio <luonghaonam99@gmail.com>
2019-10-24 08:21:14 +0700

    San pham

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './components/Persons/Person';
+import Sanpham from './components/Sanpham/Sanpham';
 
 // function App() {
 //   return (
@@ -24,16 +25,42 @@ import Person from './components/Persons/Person';
 //   );
 // }
 
+// class App extends Component {
+//   render(){
+//     return (
+//       <div className="App">
+//         <h1>My first React App</h1>
+//         <p>By myself</p>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <Person hoten="Luong Hao Nam" tuoi="20">
+//           Favorite: lady
+//         </Person>
+//       </div>
+//     )
+//   }
+// }
+
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>My first React App</h1>
-        <p>By myself</p>
+        <h1>Shop</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <Person hoten="Luong Hao Nam" tuoi="20">
-          Favorite: lady
-        </Person>
+        <Sanpham masanpham="1" tensanpham="Máy cạo râu" gia="299.000">
+          Sản phẩm cho phái mạnh
+        </Sanpham>
+        <Sanpham masanpham="2" tensanpham="Máy lọc không khí" gia="2.499.000">
+          Sản phẩm cho gia đình
+        </Sanpham>
+        <Sanpham masanpham="3" tensanpham="Máy tạo bọt" gia="149.000">
+          Sản phẩm cho gia đình
+        </Sanpham>
+        <Sanpham masanpham="4" tensanpham="Máy hút bụi" gia="639.000">
+          Sản phẩm cho gia đình
+        </Sanpham>
+        <Sanpham masanpham="5" tensanpham="Tai nghe bluetooth" gia="619.000">
+          Sản phẩm cho phái mạnh
+        </Sanpham>
       </div>
     )
   }
--- src/components/Persons/Person.js
+++ src/components/Persons/Person.js
@@ -12,5 +12,4 @@ const person = (props) => {
         </div>
     )
 }
-
 export default person
